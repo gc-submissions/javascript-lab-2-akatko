756a9ddd11a1dcfb505327eefb9bb5f0a8e9c518
akatko <kateleykohler@gmail.com>
2022-02-23 02:18:25 +0000

    corrected fight function

--- src/script.js
+++ src/script.js
@@ -1,52 +1,57 @@
 "use strict";
 
-const randomDamage = () => {
-    return Math.floor(Math.random() * 10 + 1);
-}
+let randomDamage = () => {
+  return Math.floor(Math.random() * 10 + 1);
+};
 
-console.log(randomDamage());
+let chooseOption = (opt1, opt2) => {
+  let randNum = Math.floor(Math.random() * 2);
+  return randNum === 0 ? opt1 : opt2;
+};
 
+let attackPlayer = function (health) {
+  return (health = health - randomDamage());
+};
 
-const chooseOption = (opt1, opt2) => {
-    let randNum = Math.floor(Math.random() * 2);
-    return randNum === 0 ? opt1 : opt2;
-}
+let logHealth = (player, health) => {
+  console.log(`${player} health: ${health}`);
+};
 
-const attackPlayer  = function(health) {
-    return health = health - randomDamage();
-}
+let logDeath = (winner, loser) => {
+  console.log(`${winner} defeated ${loser}`);
+};
 
-const logHealth = (player, health) => {
-    console.log (`${player} health: ${health}`);
-}
+let isDead = (health) => {
+  if (health <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-const logDeath = (winner, loser) => {
-    console.log(`${winner} defeated ${loser}`);
-}
+function fight(player1, player2, player1Health, player2Health) {
+  while (true) {
+    let attacker = chooseOption(player1, player2);
 
-const isDead = (health) => {
-    if (health <= 0) {
-        return true;
-    } else {
-        return false; 
-    }
-}
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
 
-function fight (player1, player2, player1Health, player2Health) {
- while (true) { 
-      if (attacker === player1) {
-        let attacker = chooseOption(player1, player2);
-        player2Health = attackPlayer(player2Health);
-         return logHealth(player2, player2Health); 
-    } if (isDead(player2Health)) {
+      if (isDead(player2Health) === true) {
         logDeath(player1, player2);
-         break;
-    } else player1Health = attackPlayer(player1Health) 
-       logHealth(player1, player1Health);
-
-}
+        break;
+      }
+    } else {
+      player1Health = attackPlayer(player1Health);
+      logHealth(player1, player1Health);
+      if (isDead(player1Health)) {
+        logDeath(player2, player1);
+        break;
+      }
     }
+  }
+}
 
-
+fight("Mitch", "Adam", 100, 100);
 
 // TODO - write your code here.
